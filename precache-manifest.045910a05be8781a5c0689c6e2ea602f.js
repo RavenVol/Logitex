da32675c5a11e28d3a0c9832fda6a396c239f55c
RavenVol <r.volianiuk@gmail.com>
2019-12-01 14:13:25 +0200

    Updates

---
+++ precache-manifest.045910a05be8781a5c0689c6e2ea602f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4176a30ce105f463a3532c8c3f0e7f9",
+    "url": "/Logitex/index.html"
+  },
+  {
+    "revision": "ec9d867db43e4ccf24bf",
+    "url": "/Logitex/static/css/main.6254e0c8.chunk.css"
+  },
+  {
+    "revision": "188069797842762fe8a9",
+    "url": "/Logitex/static/js/2.6379257e.chunk.js"
+  },
+  {
+    "revision": "ec9d867db43e4ccf24bf",
+    "url": "/Logitex/static/js/main.ac245847.chunk.js"
+  },
+  {
+    "revision": "74ae96fb4dc828fc6ffe",
+    "url": "/Logitex/static/js/runtime~main.2c6e21bb.js"
+  }
+]);
